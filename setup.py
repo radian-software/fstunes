8ea4e40011c9ba39d6dbc7ef8e712bdd064588bf
Radon Rosborough <radon.neon@gmail.com>
2019-01-09 12:07:22 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# https://python-packaging.readthedocs.io/en/latest/minimal.html
+setup(
+    author="Radon Rosborough",
+    author_email="radon.neon@gmail.com",
+    description="Minimal command-line music library manager and media player.",
+    license="MIT",
+    install_requires=[],
+    name="fstunes",
+    url="https://github.com/raxod502/fstunes",
+    version="0.0-dev",
+)
